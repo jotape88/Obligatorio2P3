3ad84e5eb11a5aa9f1d59b5d317957fa09e36976
Zven88 <jota171088@gmail.com>
2021-11-14 04:11:45 -0300

    modificaciones esteticas a listar cupos y pases vigentes

--- WebObligatorio2P3/Models/ViewModelPagoMensualidad.cs
+++ WebObligatorio2P3/Models/ViewModelPagoMensualidad.cs
@@ -13,15 +13,15 @@ namespace WebObligatorio_2_P3.Models
         public string TipoPago { get; set; }
         [Display(Name = "Fecha de pago"), DataType(DataType.Date)]
         public DateTime FechaPago { get; set; }
-        [Display(Name = "Monto pagado")]
+        [Display(Name = "Monto pagado"), DisplayFormat(DataFormatString = "{0:n0}")]
         public decimal MontoPago { get; set; }
-        [Display(Name = "Monto de descuento aplicado")]
+        [Display(Name = "Monto de descuento aplicado"), DisplayFormat(DataFormatString = "{0:n0}")]
         public decimal DescuentoPago { get; set; }
         [Display(Name = "Cédula del socio")]
         public string CedulaSocio { get; set; }
         [Display(Name ="Nombres y apellidos del socio")]
         public string NombreSocio { get; set; }
-        
+   
         [Required, Range(1,12, ErrorMessage = "Debe ingresar un mes válido")]
         public int Mes { get; set; } //Esto esta bien?
         [Required, Display(Name = "Año"), StringLength(4, MinimumLength = 4, ErrorMessage = "Debe ingresar un año válido")]
